63456f7f11bc617e5bdded52836f54a8ec4cecce
Chia-Jung Hsu <appolloford@gmail.com>
2021-06-10 15:50:17 +0200

    Fix bugs in GetResistivity

--- src/enzo/GetResistivity.C
+++ src/enzo/GetResistivity.C
@@ -60,75 +60,76 @@ int GetConductivity(float B, float T, float Time, float rho_HI, float rho_H2I, f
   float p_He = 0.207, p_H = 0.667, p_H2 = 0.804;
 
   float sqrtT = sqrt(T);
-  float theta = log(T);
+  float theta = log10(T);
   // printf("Tmperature = %13.7e\n", T);
 
   // collisions with e-
-  coeff_sH = 1e-9 * sqrtT * (2.841 + 0.093 * theta + 0.245 * pow(theta, 2) - 0.089 * pow(theta, 3));
+  coeff_sH = 1e-9 * sqrtT * (2.841 + 0.093 * theta + 0.245 * pow(theta, 2.0) - 0.089 * pow(theta, 3.0));
   coeff_sHe = 1e-9 * sqrtT * 0.428;
-  coeff_sH2 = 1e-9 * sqrtT * (0.535 + 0.203 * theta - 0.163 * pow(theta, 2) + 0.050 * pow(theta, 3));
+  coeff_sH2 = 1e-9 * sqrtT * (0.535 + 0.203 * theta - 0.163 * pow(theta, 2.0) + 0.050 * pow(theta, 3.0));
   // momentum exchange
   tau_sH = (me + mh) / (rho_HI * coeff_sH);
-  tau_sHe = (me + 4.0 * mh) / (rho_HeI * coeff_sH);
-  tau_sH2 = (me + 2.0 * mh) / (rho_H2I * coeff_sH);
+  tau_sHe = (me + 4.0 * mh) / (rho_HeI * coeff_sHe);
+  tau_sH2 = (me + 2.0 * mh) / (rho_H2I * coeff_sH2);
   tau_s[0] = 1.0 / (1.0 / tau_sH + 1.0 / tau_sHe + 1.0 / tau_sH2);
 
   // collisions with H+
   coeff_sH = 1e-9 * 0.649 * pow(T, 0.375);
-  coeff_sHe = 1e-9 * (1.424 + 7.438e-6 * T - 6.734e-9 * pow(T, 2));
-  coeff_sH2 = 1e-9 * (1.003 + 0.05 * theta + 0.136 * pow(theta, 2) - 0.014 * pow(theta, 3));
+  coeff_sHe = 1e-9 * (1.424 + 7.438e-6 * T - 6.734e-9 * pow(T, 2.0));
+  coeff_sH2 = 1e-9 * (1.003 + 0.05 * theta + 0.136 * pow(theta, 2.0) - 0.014 * pow(theta, 3.0));
   tau_sH = (mh + mh) / (rho_HI * coeff_sH);
-  tau_sHe = (mh + 4.0 * mh) / (rho_HeI * coeff_sH);
-  tau_sH2 = (mh + 2.0 * mh) / (rho_H2I * coeff_sH);
+  tau_sHe = (mh + 4.0 * mh) / (rho_HeI * coeff_sHe);
+  tau_sH2 = (mh + 2.0 * mh) / (rho_H2I * coeff_sH2);
   tau_s[1] = 1.0 / (1.0 / tau_sH + 1.0 / tau_sHe + 1.0 / tau_sH2);
 
   // collisions with He+
   coeff_sH = 1e-9 * 4.71e-1;
-  coeff_sHe = 1e-9 * 2.0 * 8.73e-2 * pow(1.0 - 0.093 * theta, 2);
+  coeff_sHe = 1e-9 * 2.0 * 8.73e-2 * pow(1.0 - 0.093 * theta, 2.0);
   // Langevin approximation H -> H2
-  coeff_sH2 = sqrt((4.0 + 2.0) * 1.0 * p_H2 / (4.0 + 1.0) * 2.0 * p_H) * tau_sH;
+  coeff_sH2 = sqrt((4.0 + 2.0) * 1.0 * p_H2 / (4.0 + 1.0) * 2.0 * p_H) * coeff_sH;
   tau_sH = (4.0 * mh + mh) / (rho_HI * coeff_sH);
-  tau_sHe = (4.0 * mh + 4.0 * mh) / (rho_HeI * coeff_sH);
-  tau_sH2 = (4.0 * mh + 2.0 * mh) / (rho_H2I * coeff_sH);
+  tau_sHe = (4.0 * mh + 4.0 * mh) / (rho_HeI * coeff_sHe);
+  tau_sH2 = (4.0 * mh + 2.0 * mh) / (rho_H2I * coeff_sH2);
   tau_s[2] = 1.0 / (1.0 / tau_sH + 1.0 / tau_sHe + 1.0 / tau_sH2);
 
   // collisions with H3+
-  coeff_sH2 = 1e-9 * (2.693 - 1.238 * theta + 0.663 * pow(theta, 2) - 0.089 * pow(theta, 3));
+  coeff_sH2 = 1e-9 * (2.693 - 1.238 * theta + 0.663 * pow(theta, 2.0) - 0.089 * pow(theta, 3.0));
   // Langevin approximation H2 -> H
-  coeff_sH = sqrt((3.0 + 1.0) * 2.0 * p_H / (3.0 + 2.0) * 1.0 * p_H2) * tau_sH2;
+  coeff_sH = sqrt((3.0 + 1.0) * 2.0 * p_H / (3.0 + 2.0) * 1.0 * p_H2) * coeff_sH2;
   // Langevin approximation H2 -> He
-  coeff_sHe = sqrt((3.0 + 4.0) * 2.0 * p_He / (3.0 + 2.0) * 4.0 * p_H2) * tau_sH2;
+  coeff_sHe = sqrt((3.0 + 4.0) * 2.0 * p_He / (3.0 + 2.0) * 4.0 * p_H2) * coeff_sH2;
   tau_sH = (3.0 * mh + mh) / (rho_HI * coeff_sH);
-  tau_sHe = (3.0 * mh + 4.0 * mh) / (rho_HeI * coeff_sH);
-  tau_sH2 = (3.0 * mh + 2.0 * mh) / (rho_H2I * coeff_sH);
+  tau_sHe = (3.0 * mh + 4.0 * mh) / (rho_HeI * coeff_sHe);
+  tau_sH2 = (3.0 * mh + 2.0 * mh) / (rho_H2I * coeff_sH2);
   tau_s[3] = 1.0 / (1.0 / tau_sH + 1.0 / tau_sHe + 1.0 / tau_sH2);
 
   // collisions with molecular ions (approximation from HCO+)
-  coeff_sH2 = 1e-9 * sqrtT * (1.476 - 1.409 * theta + 0.555 * pow(theta, 2) - 0.0775 * pow(theta, 3));
+  coeff_sH2 = 1e-9 * sqrtT * (1.476 - 1.409 * theta + 0.555 * pow(theta, 2.0) - 0.0775 * pow(theta, 3.0));
   // Langevin approximation H2 -> H
-  coeff_sH = sqrt((29.0 + 1.0) * 2.0 * p_H / (29.0 + 2.0) * 1.0 * p_H2) * tau_sH2;
+  coeff_sH = sqrt((29.0 + 1.0) * 2.0 * p_H / (29.0 + 2.0) * 1.0 * p_H2) * coeff_sH2;
   // Langevin approximation H2 -> He
-  coeff_sHe = sqrt((29.0 + 4.0) * 2.0 * p_He / (29.0 + 2.0) * 4.0 * p_H2) * tau_sH2;
+  coeff_sHe = sqrt((29.0 + 4.0) * 2.0 * p_He / (29.0 + 2.0) * 4.0 * p_H2) * coeff_sH2;
   tau_sH = (29.0 * mh + mh) / (rho_HI * coeff_sH);
-  tau_sHe = (29,0 * mh + 4.0 * mh) / (rho_HeI * coeff_sH);
-  tau_sH2 = (29.0 * mh + 2.0 * mh) / (rho_H2I * coeff_sH);
+  tau_sHe = (29.0 * mh + 4.0 * mh) / (rho_HeI * coeff_sHe);
+  tau_sH2 = (29.0 * mh + 2.0 * mh) / (rho_H2I * coeff_sH2);
   tau_s[4] = 1.0 / (1.0 / tau_sH + 1.0 / tau_sHe + 1.0 / tau_sH2);
 
   // collisions with atomic ions (approximation from C+)
-  coeff_sH = 1e-9 * (1.983 + 0.425 * theta - 0.431 * pow(theta, 2) + 0.114 * pow(theta, 3));
+  coeff_sH = 1e-9 * (1.983 + 0.425 * theta - 0.431 * pow(theta, 2.0) + 0.114 * pow(theta, 3.0));
   // Langevin approximation H -> H2
-  coeff_sH2 = sqrt((12.0 + 2.0) * 1.0 * p_H2 / (12.0 + 1.0) * 2.0 * p_H) * tau_sH;
+  coeff_sH2 = sqrt((12.0 + 2.0) * 1.0 * p_H2 / (12.0 + 1.0) * 2.0 * p_H) * coeff_sH;
   // Langevin approximation H -> He
-  coeff_sHe = sqrt((12.0 + 4.0) * 1.0 * p_He / (12.0 + 1.0) * 4.0 * p_H) * tau_sH;
+  coeff_sHe = sqrt((12.0 + 4.0) * 1.0 * p_He / (12.0 + 1.0) * 4.0 * p_H) * coeff_sH;
   tau_sH = (12.0 * mh + mh) / (rho_HI * coeff_sH);
-  tau_sHe = (12.0 * mh + 4.0 * mh) / (rho_HeI * coeff_sH);
-  tau_sH2 = (12.0 * mh + 2.0 * mh) / (rho_H2I * coeff_sH);
+  tau_sHe = (12.0 * mh + 4.0 * mh) / (rho_HeI * coeff_sHe);
+  tau_sH2 = (12.0 * mh + 2.0 * mh) / (rho_H2I * coeff_sH2);
   tau_s[5] = 1.0 / (1.0 / tau_sH + 1.0 / tau_sHe + 1.0 / tau_sH2);
 
   // printf("tau e: %13.7e, H+: %13.7e, He+: %13.7e, H3+: %13.7e, M+: %13.7e, A+: %13.7e\n", 
   //         tau_s[0], tau_s[1], tau_s[2], tau_s[3], tau_s[4], tau_s[5]);
 
-  const float ucharge = 4.80320425e-10; // statcoulombs = cm^(3/2) g^(1/2) s^-1
+  // const float ucharge = 4.80320425e-10; // statcoulombs = cm^(3/2) g^(1/2) s^-1
+  const float ucharge = 1.6e-19; // statcoulombs = cm^(3/2) g^(1/2) s^-1
 
   sigma_s[0] = (rho_E / mh) * tau_s[0] * ucharge * ucharge / me;                     // conductivity of e-, n_e = rho_e/mh
   sigma_s[1] = (rho_HII / mh) * tau_s[1] * ucharge * ucharge / mh;                   // H+
@@ -151,12 +152,21 @@ int GetConductivity(float B, float T, float Time, float rho_HI, float rho_H2I, f
   // printf("omega e: %13.7e, H+: %13.7e, He+: %13.7e, H3+: %13.7e, M+: %13.7e, A+: %13.7e\n", 
   //         omega_s[0], omega_s[1], omega_s[2], omega_s[3], omega_s[4], omega_s[5]);
 
+  // double n_s[6] = {0.0};
+  // n_s[0] = (rho_E / mh);
+  // n_s[1] = (rho_HII / mh);
+  // n_s[2] = (rho_HeII / (4.0 * mh));
+  // n_s[3] = (rho_H3II / (3.0 * mh));
+  // n_s[4] = (rho_MII / (29.0 * mh));
+  // n_s[5] = (rho_AII / (12.0 * mh));
+
   cond[0] = cond[1] = cond[2] = 0.0;
   for (int i = 0; i < 6; i++)
   {
+    // printf("tau, sigma, omega, n %d: %13.7e, %13.7e, %13.7e, %13.7e\n", i, tau_s[i], sigma_s[i], omega_s[i], n_s[i]);
     cond[0] += sigma_s[i];
-    cond[1] += sigma_s[i] / (1.0 + pow(omega_s[i] * tau_s[i], 2));
-    cond[2] -= sigma_s[i] * omega_s[i] * tau_s[i] / (1.0 + pow(omega_s[i] * tau_s[i], 2));
+    cond[1] += sigma_s[i] / (1.0 + pow(omega_s[i] * tau_s[i], 2.0));
+    cond[2] -= sigma_s[i] * omega_s[i] * tau_s[i] / (1.0 + pow(omega_s[i] * tau_s[i], 2.0));
   }
 
   return SUCCESS;
@@ -185,6 +195,8 @@ float GetResistivity(float rho, float B, float T, float Time,
 
   rho *= DensityUnits;
   B *= MagneticUnits;
+  // printf("Units Density, Length, Temperature, Time, Velocity, Magnetic: %13.7e, %13.7e, %13.7e, %13.7e, %13.7e, %13.7e\n", 
+  //         DensityUnits, LengthUnits, TemperatureUnits, TimeUnits, VelocityUnits, MagneticUnits);
 
   rho_HI *= DensityUnits;
   rho_H2I *= DensityUnits;
@@ -207,6 +219,8 @@ float GetResistivity(float rho, float B, float T, float Time,
     }
   }
 
+  float ni, nn, eta_perp4, v_table;
+  float cond[3], eta[3], eta_AD;
   switch (ADResistivityType)
   {
   case 1:
@@ -217,12 +231,54 @@ float GetResistivity(float rho, float B, float T, float Time,
     eta_perp *= TimeUnits / (LengthUnits * LengthUnits);
     return eta_perp;
   case 3:
+    rho = 100.0 * mh;
+    v_table = ResistivityTableLookup(rho, T, B);
     eta_perp = clight_code * clight_code * ResistivityTableLookup(rho, T, B) / (4. * pi);
     eta_perp /= TimeUnits;
     // printf("ADResistivityType = 3, eta = %13.7e\n", eta_perp);
+
+    // // case 4:
+    // nn = rho / (2.33 * 1.6733e-24);
+    // ni = 1e-3 * pow(nn / 1e5, 0.5) + 1e-3 * pow(nn / 1e3, -2);
+    // rhoi = 29.0 * 1.6733e-24 * ni;
+    // alpha = 3.7e13;
+    // eta_perp4 = B * B / (4.0 * pi * alpha * rhoi * rho);
+    // eta_perp4 *= TimeUnits / (LengthUnits * LengthUnits);
+
+    // // rho_HI = 1.7642416352609227e-23;
+    // // rho_H2I = 1.4961574035528677e-22;
+    // // rho_HeI = 6.690021147476558e-23;
+    // // rho_E = 2.1302415233900016e-26;
+    // // rho_HII = 3.9271668227270314e-27;
+    // // rho_HeII = 4.6572828181708464e-27;
+    // // rho_H3II = 6.114702491862175e-30;
+    // // rho_MII = 2.5308375224023457e-33;
+    // // rho_AII = 1.945047865765666e-25;
+
+    // // rhoi = rho_HII + rho_HeII + rho_H3II + rho_MII + rho_AII;
+    // // alpha = 3.7e13;
+    // // eta_perp4 = B * B / (4.0 * pi * alpha * rhoi * rho);
+    // // eta_perp4 *= TimeUnits / (LengthUnits * LengthUnits);
+
+    // // case 5:
+    // GetConductivity(B, T, Time, rho_HI, rho_H2I, rho_HeI,
+    //                 rho_E, rho_HII, rho_HeII, rho_H3II,
+    //                 rho_MII, rho_AII, &cond[0]);
+
+    // printf("rho, B, T, table: %13.7e, %13.7e, %13.7e, %13.7e\n", rho, B, T, v_table);
+    // printf("%13.7e, %13.7e, %13.7e, %13.7e, %13.7e, %13.7e, %13.7e, %13.7e, %13.7e\n", rho_HI, rho_H2I, rho_HeI, rho_E, rho_HII, rho_HeII, rho_H3II, rho_MII, rho_AII);
+    // printf("%13.7e, %13.7e, %13.7e\n", cond[0], cond[1], cond[2]);
+
+    // eta[0] = 1.0 / cond[0];                                     // eta_parallel
+    // eta[1] = cond[1] / (cond[1] * cond[1] + cond[2] * cond[2]); // eta_perpendicular
+    // eta_AD = eta[1] - eta[0];
+    // printf("eta_para, eta_perp, eta_ad: %13.7e, %13.7e, %13.7e\n", eta[0], eta[1], eta_AD);
+    // eta_AD *= clight * clight / (4.0 * pi);
+    // eta_AD *= TimeUnits / (LengthUnits * LengthUnits);
+    // printf("Type3: %13.7e, Type4: %13.7e, Type5: %13.7e\n", eta_perp, eta_perp4, eta_AD);
+
     return eta_perp;
   case 4:
-    float ni, nn;
     nn = rho / (2.33 * 1.6733e-24);
     ni = 1e-3 * pow(nn / 1e5, 0.5) + 1e-3 * pow(nn / 1e3, -2);
     rhoi = 29.0 * 1.6733e-24 * ni;
@@ -232,7 +288,6 @@ float GetResistivity(float rho, float B, float T, float Time,
     // printf("ADResistivityType = 4, eta = %13.7e\n", eta_perp);
     return eta_perp;
   case 5:
-    float cond[3], eta[3], eta_AD;
     GetConductivity(B, T, Time, rho_HI, rho_H2I, rho_HeI,
                     rho_E, rho_HII, rho_HeII, rho_H3II,
                     rho_MII, rho_AII, &cond[0]);
